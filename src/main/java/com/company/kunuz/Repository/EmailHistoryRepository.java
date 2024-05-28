eb55d3b2bab15b424e924f92c40ac8f29b0bc0af
DizzzY0 <20041017i@gmail.com>
2024-05-28 18:40:30 +0500

    Second Commit

---
+++ src/main/java/com/company/kunuz/Repository/EmailHistoryRepository.java
@@ -0,0 +1,21 @@
+package com.company.kunuz.Repository;
+
+
+
+import com.company.kunuz.Entity.EmailHistoryEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+
+public interface EmailHistoryRepository extends CrudRepository<EmailHistoryEntity, Integer> {
+
+    Long countByEmailAndCreatedDateBetween(String email, LocalDateTime from, LocalDateTime to);
+
+    // select count(*) from email_history createdDate between :from and :to
+    @Query("from EmailHistoryEntity order by createdDate desc limit 1")
+    Optional<EmailHistoryEntity> findLastByEmail(String email);
+}
---
+++ src/main/java/com/company/kunuz/Service/SmsHistoryService.java
@@ -0,0 +1,53 @@
+package com.company.kunuz.Service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class SmsHistoryService {
+    @Autowired
+    private SmsHistoryRepository smsHistoryRepository;
+
+
+    public void crete(String toPhone, String text) {
+
+        SmsHistoryEntity entity = new SmsHistoryEntity();
+
+        entity.setPhone(toPhone);
+        entity.setMessage(text);
+
+        smsHistoryRepository.save(entity);
+    }
+
+    public void checkPhoneLimit(String phone) { // 1 minute -3 attempt
+        // 23/05/2024 19:01:13
+        // 23/05/2024 19:01:23
+        // 23/05/2024 19:01:33
+
+        // 23/05/2024 19:00:55 -- (current -1)
+        // 23/05/2024 19:01:55 -- current
+
+        LocalDateTime to = LocalDateTime.now();
+        LocalDateTime from = to.minusMinutes(2);
+
+        long count = smsHistoryRepository.countByPhoneAndCreatedDateBetween(phone, from, to);
+        if (count >= 3) {
+            throw new AppBadException("Sms limit reached. Please try after some time");
+        }
+    }
+
+    public void isNotExpiredPhone(String phone) {
+        Optional<SmsHistoryEntity> optional = smsHistoryRepository.findLastByPhone(phone);
+        if (optional.isEmpty()) {
+            throw new AppBadException("Sms history not found");
+        }
+        SmsHistoryEntity entity = optional.get();
+        if (entity.getCreatedDate().plusDays(1).isBefore(LocalDateTime.now())) {
+            throw new AppBadException("Confirmation time expired");
+        }
+    }
+}
